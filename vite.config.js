49358bf7d53237f30b4d80a29005a3e89cbfb74b
KharchenkoMo <KharchenkoMo@zdrav.mos.ru>
2023-06-08 22:48:02 +0700

    added vite server config

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,14 @@ import laravel from 'laravel-vite-plugin';
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: ['resources/css/app.scss', 'resources/js/app.js'],
             refresh: true,
         }),
     ],
+    server: {
+        https: false,
+        host: true,
+        port: 5173,
+        hmr: {host: 'localhost', protocol: 'ws'},
+    },
 });
